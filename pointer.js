f0416fdf4cdc1edac44b89ad95e38d42bc52c2bc
PlatoSys <platoniinjgia@gmail.com>
2019-10-15 01:09:51 +0400

    1 click

--- pointer.js
+++ pointer.js
@@ -1,4 +1,15 @@
-function myFunction() {
+
+
+var myFunc = function func(){
+    ;
+    if( myFunc.fired ) return;
+    myFunc.fired = true;
+    alert('Your Gay Point Is'); 
     document.getElementById("demo").innerHTML =
-    Math.floor(Math.random() * 100);
-    }
+    Math.floor(Math.random() * 100)
+
+};
+
+((refToMyfunc)=>{
+    setInterval(refToMyfunc, 1000);
+   })(myFunc)
